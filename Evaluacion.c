4dfcf1b1f93e05976912d920c43b460c88493158
Mati <amaticc03@gmail.com>
2023-05-03 15:20:33 -0500

    Se agrega el proyecto con las primeras lineas de codigo en el que se declararon las variables y se le pidio al usuario el numero que desea transformar

---
+++ Evaluacion.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+
+int main(){
+
+int num1, hexa;//declaro las variables globales que se van a nesesitar
+
+char hexal
+//solicitamos el valor que el usuario desea transformar
+printf("Ingrese el numero que desea transformar en hexadecimal");
+
+scanf(%i,&num1)
+
+
+
+
+
+}
